e2ba70ec11c7832b62d391cb5a14aac9c8375abc
barsa <barsa@TEMP>
2025-10-28 01:32:10 +0300

    Реализовал сортировку

---
+++ LR2/Lab/Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var clients = new List<Client>
+        {
+            new Client { Name = "Иванов", Services = new List<string> { "стрижка", "укладка" } },
+            new Client { Name = "Петрова", Services = new List<string> { "маникюр" } }
+        };
+
+        Console.Write("Введите услугу: ");
+        string service = Console.ReadLine();
+
+        var results = clients
+            .Select(c => new { c.Name, Count = c.Services.Count(s => s == service) })
+            .Where(x => x.Count > 0)
+            .OrderByDescending(x => x.Count)
+            .ToList();
+
+        Console.WriteLine("\nРезультаты:");
+        foreach (var client in results)
+        {
+            Console.WriteLine($"{client.Name} - {client.Count} раз");
+        }
+
+        Console.ReadKey();
+    }
+}
