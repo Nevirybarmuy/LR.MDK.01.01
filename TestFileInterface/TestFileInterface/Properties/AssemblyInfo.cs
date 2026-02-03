cdcef4d3aaeb9192e990b1b2539ecf5a0f8ce7f4
Anon1m4k <kirilvalera2@gmail.com>
2026-02-03 08:45:00 +0300

    Создал проект "Тестирование файла интерфейса"

--- TestFileInterface/TestFileInterface/Properties/AssemblyInfo.cs
+++ TestFileInterface/TestFileInterface/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("TestFileInterface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("TestFileInterface")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("4589c96d-477a-4186-a467-2f7b3c2d3059")]
+[assembly: Guid("8590f494-7c1c-40f9-8433-8b4c6f59f5bb")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
